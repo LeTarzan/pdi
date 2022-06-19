7522e05daf5a68f22b501378946cabcfec01e09d
letarzan <leandrorevolve@gmail.com>
2022-06-19 20:15:20 -0300

    chore(infra): include ecs with fargate

--- terraform/production.tf
+++ terraform/production.tf
@@ -12,3 +12,40 @@ module "networking" {
   availability_zones   = local.production_availability_zones
   key_name             = "production_key"
 }
+
+module "rds" {
+  source            = "./modules/rds"
+  environment       = "production"
+  allocated_storage = "20"
+  database_name     = var.production_database_name
+  database_username = var.production_database_username
+  database_password = var.production_database_password
+  subnet_ids        = module.networking.private_subnets_id
+  vpc_id            = module.networking.vpc_id
+  instance_class    = "db.t2.micro"
+  depends_on = [
+    module.networking
+  ]
+}
+
+module "ecs" {
+  source             = "./modules/ecs"
+  environment        = "production"
+  vpc_id             = module.networking.vpc_id
+  availability_zones = local.production_availability_zones
+  repository_name    = "pdi/production"
+  subnets_ids        = module.networking.private_subnets_id
+  public_subnet_ids  = module.networking.public_subnets_id
+  security_groups_ids = [
+    module.networking.security_groups_ids,
+    module.rds.db_access_sg_id
+  ]
+  database_endpoint = module.rds.rds_address
+  database_name     = var.production_database_name
+  database_username = var.production_database_username
+  database_password = var.production_database_password
+  secret_key_base   = var.production_secret_key_base
+  depends_on = [
+    module.networking
+  ]
+}
